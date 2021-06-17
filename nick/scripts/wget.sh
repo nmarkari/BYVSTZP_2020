fea9510710e714404682ff76cd08b7dc6d284436
nmarkari <nmarkari@usc.edu>
2021-06-17 11:12:49 -0700

    fixed bug

--- nick/scripts/wget.sh
+++ nick/scripts/wget.sh
@@ -2,7 +2,7 @@
 
 #convert NeMO manifest (which did not work with portal_client) to a tsv of URL's for wget
 
-for manifest_wget in "../data_manifests/Dong-Wook/NeMO_manifest_DW_SMARTseqv4_wget.tsv"
+for manifest_wget in "../data_manifests/Dong-Wook/DW_SS4_VMH_manifest_wget.tsv" "../data_manifests/Dong-Wook/DW_SS4_VMHvl_manifest_wget.tsv"
 do
 	while read -r URL
 	do

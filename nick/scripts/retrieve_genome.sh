5f86ab0cc73111c643c4c56f0675089c890ea8d4
nmarkari <nmarkari@usc.edu>
2021-06-18 11:16:24 -0700

    adding retrieve_genome.sh

---
+++ nick/scripts/retrieve_genome.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#retrieve grcm39 genome and gene annotations
+
+OUT="../../data/ref/"
+
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/annotation_releases/10090/109/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.fna.gz -P $OUT
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/annotation_releases/10090/109/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.gtf.gz -P $OUT

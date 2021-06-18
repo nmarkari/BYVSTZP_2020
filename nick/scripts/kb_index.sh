44b1bb63a1d22f03cf056eec90e1baccdb2d6fa1
nmarkari <nmarkari@usc.edu>
2021-06-18 14:44:28 -0700

    adding kb_index.sh

---
+++ nick/scripts/kb_index.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#retrieve grcm39 genome and gene annotations
+
+kb ref -i ../../data/ref/index.idx, -g ../../data/ref/t2g.txt -f1 ../../data/ref/transcriptome.fa ../../data/ref/genome.fa ../../data/ref/genes.gtf

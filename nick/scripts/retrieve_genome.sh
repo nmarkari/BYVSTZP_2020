a3e561e3ea56947e7fab4e956d263b5b9591d500
nmarkari <nmarkari@usc.edu>
2021-06-18 14:34:51 -0700

    bug

--- nick/scripts/retrieve_genome.sh
+++ nick/scripts/retrieve_genome.sh
@@ -7,5 +7,5 @@ OUT="../../data/ref/"
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/annotation_releases/10090/109/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.fna.gz -P $OUT
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/annotation_releases/10090/109/GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.gtf.gz -P $OUT
 
-gunzip -c ${OUT}GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.fna.gz > $OUT/genome.fa
-gunzip -c ${OUT}GCF_000001635.27_GRCm39/GCF_000001635.27_GRCm39_genomic.gtf.gz > $OUT/genome.gtf
+gunzip -c ${OUT}GCF_000001635.27_GRCm39_genomic.fna.gz > $OUT/genome.fa
+gunzip -c ${OUT}GCF_000001635.27_GRCm39_genomic.gtf.gz > $OUT/genome.gtf

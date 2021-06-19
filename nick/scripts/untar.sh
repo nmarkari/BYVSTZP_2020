d6ff00f9b6f3790bc696fff55a59b96ccc3d5255
nmarkari <nmarkari@usc.edu>
2021-06-18 17:13:19 -0700

    bug

--- nick/scripts/untar.sh
+++ nick/scripts/untar.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 
-for f in ../../data/raw/DW/SS4/VMH/TEST/*.tar; do tar xf ../../data/raw/DW/SS4/VMH/TEST/"$f"; done
+for f in ../../data/raw/DW/SS4/VMH/TEST/*.tar
+	do tar xf ../../data/raw/DW/SS4/VMH/TEST/00000"$f"
+done
 
 

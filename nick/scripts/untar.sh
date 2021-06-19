43287a9065a6c08a3c98c5df415c987bf6e8869a
nmarkari <nmarkari@usc.edu>
2021-06-18 17:29:54 -0700

    fixed untar.sh

--- nick/scripts/untar.sh
+++ nick/scripts/untar.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 
-for f in ../../data/raw/DW/SS4/VMH/TEST/*.tar
+for f in ../../data/raw/DW/SS4/VMH/*.tar
 do 
-	tar -xvf $f -C ../../data/raw/DW/SS4/VMH/TEST
+	tar -xvf $f -C ../../data/raw/DW/SS4/VMH/
 	rm $f
 done
 
+for f in ../../data/raw/DW/SS4/VMHvl/*.tar
+do 
+	tar -xvf $f -C ../../data/raw/DW/SS4/VMHvl/
+	rm $f
+done
 

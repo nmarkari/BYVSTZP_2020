32dc48ab2a8f2e4030e76bc3329078ce326a5c52
nmarkari <nmarkari@usc.edu>
2021-06-18 17:10:59 -0700

    made a test untar script

--- nick/scripts/kb_count.sh
+++ nick/scripts/kb_count.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
 kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed
 -f1 ../../data/ref/transcriptome.fa ../../data/ref/genome.fa ../../data/ref/genes.gtf
---
+++ nick/scripts/untar.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+for f in ../../data/raw/DW/SS4/VMH/TEST/*.tar; do tar xf ../../data/raw/DW/SS4/VMH/TEST/"$f"; done
+
+

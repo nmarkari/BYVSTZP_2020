22a93b5362251e156515879cefffc9ddf2fa9b98
nmarkari <nmarkari@usc.edu>
2021-06-20 11:36:20 -0700

    adding gunzip to untar.sh

--- nick/scripts/kb_count.sh
+++ nick/scripts/kb_count.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed
--f1 ../../data/ref/transcriptome.fa ../../data/ref/genome.fa ../../data/ref/genes.gtf
+kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4
+-f1 ../../data/ref/transcriptome.fa ../../data/ref/genome.fa ../../data/ref/genes.gtf/
--- nick/scripts/untar.sh
+++ nick/scripts/untar.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+#untar
+#for f in ../../data/raw/DW/SS4/VMH/*.tar
+#do 
+#	tar -xvf $f -C ../../data/raw/DW/SS4/VMH/
+#	rm $f
+#done
 
-for f in ../../data/raw/DW/SS4/VMH/*.tar
+for d in ../../data/raw/DW/SS4/VMH/TEST
 do 
-	tar -xvf $f -C ../../data/raw/DW/SS4/VMH/
-	rm $f
+	gunzip -r $d -C $d
 done
 
-for f in ../../data/raw/DW/SS4/VMHvl/*.tar
-do 
-	tar -xvf $f -C ../../data/raw/DW/SS4/VMHvl/
-	rm $f
-done
+#for f in ../../data/raw/DW/SS4/VMHvl/*.tar
+#do 
+#	tar -xvf $f -C ../../data/raw/DW/SS4/VMHvl/
+#	rm $f
+#done
 

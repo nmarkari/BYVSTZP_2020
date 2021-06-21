6dc59b3164b09d3fd8aba559fdc29fdc10aec0ff
nmarkari <nmarkari@usc.edu>
2021-06-21 11:58:05 -0700

    testing kb_count on VMH data

---
+++ nick/scripts/convert_DW_cluster_metadata.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+https://data.mendeley.com/public-files/datasets/ypx3sw2f7c/files/c6138323-c7bc-4a85-86e9-add61912eb06/file_downloaded
+
--- nick/scripts/kb_count.sh
+++ nick/scripts/kb_count.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4
--f1 ../../data/ref/transcriptome.fa ../../data/ref/genome.fa ../../data/ref/genes.gtf/
+kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4/VMH --h5ad ../../data/raw/DW/SS4/VMH/*/*.fastq

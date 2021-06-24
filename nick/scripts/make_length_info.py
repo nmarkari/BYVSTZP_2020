f451121e3048139355b2b0438690967f3b030f6e
nmarkari <nmarkari@usc.edu>
2021-06-24 11:37:39 -0700

    fixed lengths by adding line.strip() for reads

--- nick/scripts/make_length_info.py
+++ nick/scripts/make_length_info.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
 import csv
 with open ('../../data/ref/length_info.txt','w') as ofile:
-    tsvwriter = csv.reader(f2,delimiter="\t")
+    tsvwriter = csv.writer(ofile,delimiter="\t")
     f = open('../../data/ref/transcriptome.fa','r')
     lines = f.readlines()
     seq = False
     info = []
-    for i in lines:
-        if seq == False and not line.startswith('>')
-            print("error: "+line)
+    for line in lines:
+        if seq == False:
+            if not line.startswith('>'):
+                print("error: "+line)
             line = line[1:]
             info = line.split()
             seq = True
-        if seq == True and line.startswith('>')
-            print("error: "+line)
-            length = len(line)
-            tsvwriter.writerow(str(length),info[0],info[1],info[2],info[3],info[4],info[5],info[6],info[7])
-            
-        
-
+        else:
+            if line.startswith('>'):
+                print("error: "+line)
+            length = len(line.strip())
+            newline = [str(length)] + info
+            tsvwriter.writerow(newline)
+            info = []
+            seq = False

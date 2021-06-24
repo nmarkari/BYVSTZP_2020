d36d338cce801020c77ab9d569df77f2423a52a5
nmarkari <nmarkari@usc.edu>
2021-06-24 14:11:17 -0700

    changing kb count to 16 threads

--- nick/scripts/kb_count.sh
+++ nick/scripts/kb_count.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4/VMH --h5ad ../../data/raw/DW/SS4/VMH/*/*.fastq
-kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4/VMHvl --h5ad ../../data/raw/DW/SS4/VMHvl/*/*.fastq
+kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4/VMH -t 16 --h5ad ../../data/raw/DW/SS4/VMH/*/*.fastq
+kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4/VMHvl -t 16 --h5ad ../../data/raw/DW/SS4/VMHvl/*/*.fastq

dbc7d640e606d30bab366cd4e645e856a584108b
nmarkari <nmarkari@usc.edu>
2021-06-29 00:06:20 -0700

    fixed output directory for kb count

--- nick/scripts/kb_count.sh
+++ nick/scripts/kb_count.sh
@@ -5,5 +5,5 @@
 ~/usr/local/bin/kallisto pseudo --quant -i ../../data/ref/index.idx -o ../../data/preprocessed/DW/SS4/VMH_output  -b ../../data/preprocessed/DW/SS4/VMH/batch.txt -t 16
 ~/usr/local/bin/kallisto pseudo --quant -i ../../data/ref/index.idx -o ../../data/preprocessed/DW/SS4/VMHvl_output  -b ../../data/preprocessed/DW/SS4/VMHvl/batch.txt -t 16
 
-kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4/VMH -t 16 --h5ad ../../data/preprocessed/DW/SS4/VMH/batch.txt
-kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4/VMHvl -t 16 --h5ad ../../data/preprocessed/DW/SS4/VMHvl/batch.txt
+kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4/VMH_output -t 16 --h5ad ../../data/preprocessed/DW/SS4/VMH/batch.txt
+kb count -i ../../data/ref/index.idx -g ../../data/ref/t2g.txt -x SMARTSEQ -o ../../data/preprocessed/DW/SS4/VMHvl_output -t 16 --h5ad ../../data/preprocessed/DW/SS4/VMHvl/batch.txt

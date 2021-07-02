1efee08424f4fde5d54d8cd74d23ddd1def22ad3
nmarkari <nmarkari@usc.edu>
2021-07-01 17:00:39 -0700

    fixed bug in manifest2tsv

--- nick/scripts/manifest_2_tsv.sh
+++ nick/scripts/manifest_2_tsv.sh
@@ -6,7 +6,7 @@ for manifest in "../../nick/data_manifests/Yao/Yao_SS4_ACA.csv"
 do
 	IN="${manifest}"
 	#OUT="${manifest}_wget.tsv"
-	OUT = "../../nick/data_manifests/Yao/Yao_SS4_ACA_wget.tsv"
+	OUT="../../nick/data_manifests/Yao/Yao_SS4_ACA_wget.tsv"
 	#removing quotation marks, comma so just the URL is left
 	awk '{s=index($0,"http");e=index($0,".tar");print substr($0,s,e+4-s)}' $IN > $OUT
 done

113970ba0ae1f1d699981eacf62d949447de03b5
nmarkari <nmarkari@usc.edu>
2021-07-12 08:17:31 -1000

    adding -meta to fetch_and_preprocess.sh

--- nick/scripts/fetch_and_preprocess.sh
+++ nick/scripts/fetch_and_preprocess.sh
@@ -12,23 +12,23 @@
 #        arr.push(l[i].href);
 #    }
 #}
-echo 'BEGIN manifest_2_tsv.sh'
-bash manifest_2_tsv.sh
-echo 'BEGIN WGET.SH'
-bash wget.sh
-echo 'BEGIN untar'
-bash untar.sh
+#echo 'BEGIN manifest_2_tsv.sh'
+#bash manifest_2_tsv.sh
+#echo 'BEGIN WGET.SH'
+#bash wget.sh
+#echo 'BEGIN untar'
+#bash untar.sh
 
 
 
 #retrieve reference genome, use kb to preprocess data
-echo 'BEGIN retrieve_genome.sh'
-bash retrieve_genome.sh
-echo 'BEGIN kb_index.sh'
-bash kb_index.sh
-echo 'BEGIN kallisto_count.sh'
-bash kallisto_count.sh
-echo 'BEGIN make_length_info.py'
-python3 make_length_info.py
+#echo 'BEGIN retrieve_genome.sh'
+#bash retrieve_genome.sh
+#echo 'BEGIN kb_index.sh'
+#bash kb_index.sh
+#echo 'BEGIN kb_count.sh'
+#bash kb_count.sh
+#echo 'BEGIN make_length_info.py'
+#python3 make_length_info.py
 echo 'BEGIN make_adata.py'
-./make_adata.py -m '../../data/preprocessed/Yao/SS4/ACA/matrix.abundance.mtx' -c '../../data/preprocessed/Yao/SS4/ACA/matrix.cells' -t '../../data/preprocessed/Yao/SS4/ACA/transcripts.txt' --outdir '../../data/preprocessed/Yao/SS4/ACA'
+./make_adata.py -m '../../data/preprocessed/Yao/SS4/ACA/matrix.abundance.mtx' -c '../../data/preprocessed/Yao/SS4/ACA/matrix.cells' -t '../../data/preprocessed/Yao/SS4/ACA/transcripts.txt' -meta '../metadata_files/CTX_Hip_anno_SSv4.csv' --outdir '../../data/preprocessed/Yao/SS4/ACA'
